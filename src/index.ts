eed8f9c55c046893dc2124f515a4bcce015692c3
Alexandre Paez Sequeira <alepaezseq@gmail.com>
2021-03-14 22:03:43 -0300

    Export classes and interfaces on index.js

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import Queue from './queue/Queue';
+import Message from './queue/Message';
+import WorkerOptions from './WorkerOptions';
+
+import MemoryQueue from './queue/MemoryQueue';
+import Worker from './Worker';
+
+export {
+  Queue,
+  Message,
+  WorkerOptions,
+};
+
+export default {
+  Worker,
+  MemoryQueue,
+};
